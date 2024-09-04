cf9e01ee73abbb35104e5498d7832b2c1475af9d
PierreMorin4590 <pierremorin4590@hotmail.fr>
2024-09-05 00:02:01 +0200

    Test CORSPROXY.IO :white_check_mark:

--- sudoku/js/app.js
+++ sudoku/js/app.js
@@ -1,7 +1,7 @@
 const app = {
 
     // URL de l'API permettant de générer une grille
-    createUrl: "https://corsproxy.io/?https%3A%2F%2Fsudoku-game-and-api.netlify.app%2Fapi%2Fsudoku",
+    createUrl: "https://corsproxy.io/?https://sudoku-game-and-api.netlify.app/api/sudoku",
     contentToCopy: "",
     sudoku: null,
 
